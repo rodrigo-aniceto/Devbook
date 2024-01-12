378e9bea8137cd36fb18e907b456e2bdc1761755
Rodrigo Aniceto <rodrigo_anicetoo@yahoo.com.br>
2024-01-12 16:49:39 -0300

    remover print desnecessario

--- api/src/banco/banco.go
+++ api/src/banco/banco.go
@@ -3,14 +3,12 @@ package banco
 import (
 	"api/src/config"
 	"database/sql"
-	"fmt"
 
 	_ "github.com/go-sql-driver/mysql" //Driver
 )
 
 //Conectar abre a conexao com o banco de dados e a retorna
 func Conectar() (*sql.DB, error) {
-	fmt.Println(config.StringConexaoBanco)
 	db, erro := sql.Open("mysql", config.StringConexaoBanco)
 	if erro != nil {
 		return nil, erro
