8616a26eaf2ef6f659e802a3d8d998c3b506e14e
Rodrigo Aniceto <rodrigo_anicetoo@yahoo.com.br>
2024-01-13 11:26:16 -0300

    respostas da API

---
+++ api/src/respostas/respostas.go
@@ -0,0 +1,27 @@
+package respostas
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// JSON retorna uma resposta em JSON para a requisição
+func JSON(rw http.ResponseWriter, statusCode int, dados interface{}) {
+	rw.WriteHeader(statusCode)
+
+	if erro := json.NewEncoder(rw).Encode(dados); erro != nil {
+		log.Fatal(erro)
+	}
+
+}
+
+// ERRO retorna um erro em formato JSON
+func Erro(rw http.ResponseWriter, statusCode int, erro error) {
+	JSON(rw, statusCode, struct {
+		Erro string `json:"erro"`
+	}{
+		Erro: erro.Error(),
+	})
+
+}
