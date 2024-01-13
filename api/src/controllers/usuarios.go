68ac1fb4ba54542c465cfa3361aa8bd9a8514779
Rodrigo Aniceto <rodrigo_anicetoo@yahoo.com.br>
2024-01-13 11:24:58 -0300

    erros em formato json

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -4,32 +4,45 @@ import (
 	"api/src/banco"
 	"api/src/modelos"
 	"api/src/repositorios"
+	"api/src/respostas"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func CriarUsuario(rw http.ResponseWriter, r *http.Request) {
 	corpoRequest, erro := ioutil.ReadAll(r.Body)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(rw, http.StatusUnprocessableEntity, erro)
+		return
 	}
+
 	var usuario modelos.Usuario
 	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(rw, http.StatusBadRequest, erro)
+		return
 	}
+
+	if erro = usuario.Preparar(); erro != nil {
+		respostas.Erro(rw, http.StatusBadRequest, erro)
+		return
+	}
+
 	db, erro := banco.Conectar()
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
+		return
 	}
+	defer db.Close()
+
 	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
-	usuarioID, erro := repositorio.Criar(usuario)
+	usuario.ID, erro = repositorio.Criar(usuario)
 	if erro != nil {
-		log.Fatal(erro)
+		respostas.Erro(rw, http.StatusInternalServerError, erro)
+		return
 	}
-	rw.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioID)))
+
+	respostas.JSON(rw, http.StatusCreated, usuario)
 }
 
 func BuscarUsuarios(rw http.ResponseWriter, r *http.Request) {
--- api/src/modelos/usuario.go
+++ api/src/modelos/usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //usuario representa um usuario da rede
 type Usuario struct {
@@ -10,4 +14,39 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("o nome é obrigatório")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("o nick é obrigatório")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("o e-mail é obrigatório")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("a senha é obrigatória")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
