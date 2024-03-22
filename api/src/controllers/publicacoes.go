913b52864d4eba814c8a88a3f0e299c02ea29bdd
Rodrigo Aniceto <rodrigo_anicetoo@yahoo.com.br>
2024-03-22 12:03:56 -0300

    criacao rotas publicacoes

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+//CriarPublicacao usuario cria uma
+func CriarPublicacao(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+//BuscarPublicacao traz uma única publicação
+func BuscarPublicacao(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+//BuscarPublicacoes todas de quem  usuario segue e de si mesmo
+func BuscarPublicacoes(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+//AtualizarPublicacao atualiza
+func AtualizarPublicacao(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+//DeletarPublicacao deleta uma única
+func DeletarPublicacao(rw http.ResponseWriter, r *http.Request) {
+
+}
